c80d37b28a157c494708bd40c73c6fc8446f1f58
Davi <davifsclmt@gmail.com>
2024-03-21 22:54:47 -0300

    COmando de barra 'clear-messages' configurado

---
+++ commands/moderation/clear.-message.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("clear")
+        .setDescription("Limpa as mensagens de um canal ou de um usuário específico")
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+        .addIntegerOption(
+            option => option.setName('quantidade').setDescription("Quantidade de mensagens que deseja apagar").setRequired(true),
+        ),
+    async execute(interaction) {
+        const { channel, options } = interaction;
+        const amount = options.getInteger('quantidade');
+
+        const messages = await channel.messages.fetch({
+            limit: amount + 1,
+        });
+        const response = new EmbedBuilder().setColor(0x5fb041);
+
+        await channel.bulkDelete(amount, true).then(messages => {
+            response.setDescription(`${messages.size} mensagens foram deletadas`);
+            interaction.reply({ embeds: [response], ephemeral: true });
+        });
+    },
+};
