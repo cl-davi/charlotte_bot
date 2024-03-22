701ec0a6e074eed007308afe6a3f03a7dbf5ca91
Davi <davifsclmt@gmail.com>
2024-03-21 22:56:14 -0300

    Comando de barra '/wallet' configurado

---
+++ commands/economy/wallet.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { QuickDB } = require('quick.db');
+const db = new QuickDB();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("wallet")
+        .setDescription("Exibe a quantia de moedas que você possui")
+        .addUserOption(
+            option => option.setName("usuario").setDescription("Selecione um usuário para ver a carteira").setRequired(false)
+        ),
+    async execute(interaction) {
+        const { options } = interaction;
+
+        const user = options.getUser("usuario" || interaction.user);
+        let wallet = await db.get(`carteira_${user.id}`);
+
+        if (wallet == null) {
+            wallet = 0;
+        };
+
+        const embed = new EmbedBuilder().setColor("LuminousVividPink").setTitle("Carteira").setThumbnail(user.displayAvatarURL({ dynamic: true }));
+
+        if (user.id === interaction.user.id) {
+            interaction.reply({ embeds: [embed.setDescription(`Você possui \`${wallet}\` moedas em sua carteira`)] });
+        } else {
+            interaction.reply({ embeds: [embed.setDescription(`O membro ${user.tag} possui ${wallet} moedas em sua carteira`)] });
+        }
+    },
+};
