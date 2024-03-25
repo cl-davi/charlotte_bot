222ad74d0cf367d7e7763fdf5ce8cdb1870afd59
Davi <davifsclmt@gmail.com>
2024-03-25 12:34:58 -0300

    Comando de barra '/gpt' configurado

--- commands/utility/chatgpt.js
+++ commands/utility/chatgpt.js
@@ -1,49 +1,41 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const puppeteer = require('puppeteer');
+const { apiKeyOpenai } = require('../../config.json');
+const { OpenAI } = require('openai');
+
+const openai = new OpenAI(
+    { apiKey: apiKeyOpenai }
+);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('gpt')
-        .setDescription('Faça uma pergunta ao ChatGPT')
+        .setDescription('Faça uma pergunta ao chatgpt')
         .addStringOption(
-            option => option.setName('prompt').setDescription('Pergunta').setRequired(true),
+            (option) => option.setName('prompt').setDescription('Informe a pergunta').setRequired(true)
         ),
     async execute(interaction) {
-        const channelId = '1220368897539051621';
-        if (interaction.channel.id !== channelId) {
-            interaction.reply(`Envie essa mensagem no canal correto: ${channelId}`);
+        const channel = interaction.guild.channels.cache.get('1220368897539051621');
+        const question = interaction.options.getString('prompt');
+
+        if (interaction.channel.id !== channel.id) {
+            interaction.reply({ content: `Este comando só pode ser utilizado no canal: ${channel}`, ephemeral: true });
         } else {
-            await interaction.reply({ content: `Carregando sua resposta, aguarde um pouco` });
-
-            const { options } = interaction;
-            const prompt = options.getString('prompt');
-
-            const browser = await puppeteer.launch({ headless: true });
-            const page = await browser.newPage();
-
-            await page.goto('https://chat-app-f2d296.zapier.app/');
-
-            const textBoxSelector = 'textarea[aria-label="chatbot-user-prompt"]';
-            await page.waitForSelector(textBoxSelector);
-            await page.type(textBoxSelector, prompt);
-            await page.keyboard.press('Enter');
-            await page.waitForSelector('[data-testid="final-bot-response"] p');
-
-            let value = await page.$$eval('[data-testid="final-bot-response"]', async (elements) => {
-                return elements.map((element) => element.textContent)
+            await interaction.channel.sendTyping();
+            const completion = await openai.chat.completions.create({
+                model: 'gpt-4',
+                messages: [
+                    { role: 'system', content: 'Você foi programado para responder qualquer tipo de pergunta' },
+                    { role: 'user', content: question }
+                ]
             });
+            const response = completion.choices[0].message.content;
 
-            setTimeout(async () => {
-                if (value.length = 0) {
-                    return await interaction.editReply({ content: `Ocorreu um erro durante a geração de resposta` });
-                };
-            }, 1000);
-            await browser.close();
-            value.shift();
-
-            const embed = new EmbedBuilder().setColor('LuminousVividPink').setDescription(`\`\`\`${value.join(`\n\n\n\n`)}\`\`\``);
-
-            await interaction.editReply({ content: '', embeds: [embed] });
-        };
+            const embed = new EmbedBuilder()
+                .setColor('#DDA0DD')
+                .setTitle('ChatGPT')
+                .setDescription(`**PERGUNTA**: ${question}\n\n**RESPOSTA**: ${response}`);
+            
+            interaction.reply({ embeds: [embed.setTimestamp()] });
+        }
     },
 };
