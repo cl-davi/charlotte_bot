300b3e2dd228309c1f0ffae2cd111b4e4c8f382e
Davi <davifsclmt@gmail.com>
2024-03-25 12:32:30 -0300

    Comando de barra '/daily' atualizado

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -9,15 +9,15 @@ module.exports = {
     async execute(interaction) {
         if (!cooldowns[interaction.user.id]) cooldowns[interaction.user.id] = { lastCmd: null }; let ultCmd = cooldowns[interaction.user.id].lastCmd;
         let timeOut = ms("1 day");
-        const embed = new EmbedBuilder().setColor("LuminousVividPink").setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }));
+        const embed = new EmbedBuilder().setColor("#DDA0DD").setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }));
         if (ultCmd !== null && timeOut - (Date.now() - ultCmd) > 0) {
             let time = ms(timeOut - (Date.now() - ultCmd)); let resta = [time.seconds, "segundos"];
             if (resta[0] == 0) resta = ['alguns', 'milisegundos']; if (resta[0] == 1) resta = [time.seconds, "segundo"];
 
-            interaction.reply({ embeds: [embed.setTitle("Daily já foi resgatado").setDescription(`Espere \`${time}\` para poder resgatar seu daily novamente!`)], ephemeral: true }); return;
+            interaction.reply({ embeds: [embed.setTitle("Daily indisponível").setDescription(`Espere \`${time}\` para poder resgatar seu daily novamente!`)], ephemeral: true }); return;
         } else { cooldowns[interaction.user.id].lastCmd = Date.now() };
-        let quantia = Math.ceil(Math.random() * 50);
-        if (quantia < 0) quantia = quantia + 5;
+        let quantia = Math.ceil(Math.random() * 100);
+        if (quantia < 0) quantia = quantia + 10;
 
         await db.add(`carteira_${interaction.user.id}`, quantia);
 
