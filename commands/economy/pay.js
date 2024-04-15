c0ed8ee482715f199f16dd143f4ed2817ff03f5d
Davi <davifsclmt@gmail.com>
2024-04-15 18:28:51 -0300

    Comando de barra '/pay' configurado

--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -1,37 +1,61 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { QuickDB } = require('quick.db');
 
 const db = new QuickDB();
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('pay')
-        .setDescription('Realiza um pagamento')
+        .setName('pagar')
+        .setDescription('Realiza um pagamento em moedas')
         .addUserOption(
-            option => option.setName('usuario').setDescription('Selecione o membro').setRequired(true),
+            (option) => option.setName('membro').setDescription('Selecione o membro').setRequired(true),
         ).addNumberOption(
-            option => option.setName('quantia').setDescription('Quantia que vai enviar').setRequired(true),
+            (option) => option.setName('quantia').setDescription('Quantia que vai enviar').setRequired(true),
         ),
     async execute(interaction) {
         const { options } = interaction;
 
-        let user = options.getUser('usuario');
+        const user = options.getUser('membro');
         let amount = options.getNumber('quantia');
+        const channel = interaction.guild.channels.cache.get('1229515474799296652');
+        const icon = user.displayAvatarURL({ dynamic: true });
 
         let wallet = await db.get(`carteira_${interaction.user.id}`);
+
+        const embed = new EmbedBuilder()
+            .setTitle('Nota Fiscal')
+            .setColor('#4169E1');
 
         if (wallet === null) {
             wallet = 0;
         }
         if (user.id === interaction.user.id) {
-            await interaction.reply({ content: 'Você não pode enviar dinheiro para você mesmo.', ephemeral: true });
+            await interaction.reply({
+                embeds: [
+                    embed.setDescription('Você não pode enviar dinheiro para si mesmo.')
+                ],
+                ephemeral: true
+            });
         }
         if (amount > wallet) {
-            await interaction.reply({ content: 'Saldo insuficiente para realizar a operação de pagamento.', ephemeral: true });
+            await interaction.reply({
+                embeds: [
+                    embed
+                        .setDescription('Saldo insuficiente para realizar a operação de pagamento.')
+                ],
+                ephemeral: true
+            });
         } else {
             await db.add(`carteira_${user.id}`, amount);
             await db.sub(`carteira_${interaction.user.id}`, amount);
-            await interaction.reply({ content: `O valor de **R$ ${amount}** foi enviado para ${user} com sucesso.`, ephemeral: true });
+            await interaction.reply({ content: 'Quantia enviada com sucesso.', ephemeral: true });
+            await channel.send({
+                embeds: [
+                    embed
+                        .setDescription(`O valor de **R$${amount}** foi enviado para ${user}.`)
+                        .setThumbnail(icon)
+                ]
+            });
         };
     },
 };
