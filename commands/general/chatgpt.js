635e78df80c069907d18e45f1687aa8eec92ea32
Davi <davifsclmt@gmail.com>
2024-04-15 18:29:50 -0300

    Comando de barra '/gpt' configurado

---
+++ commands/general/chatgpt.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { apikeyOpenAi } = require('../../config.json');
+const { OpenAI } = require('openai');
+
+const openai = new OpenAI(
+    { apiKey: apikeyOpenAi }
+);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('gpt')
+        .setDescription('Faz uma pergunta ao chatgpt')
+        .addStringOption(
+            (option) => option.setName('prompt').setDescription('Informe a pergunta').setRequired(true)
+        ),
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const channel = interaction.guild.channels.cache.get('1228852696036151356');
+        const question = interaction.options.getString('prompt');
+
+        const messages = [
+            {
+                role: 'system',
+                content: 'Você é um bot desenvolvido para responder qualquer tipo de pergunta sobre qualquer assunto.'
+            },
+            {
+                role: 'user',
+                content: question
+            }
+        ];
+
+        if (interaction.channel.id !== channel.id) {
+            interaction.reply({ content: `Este comando só pode ser utilizado no canal: ${channel}`, ephemeral: true });
+        } else {
+            const completion = await openai.chat.completions.create({
+                model: 'gpt-4',
+                messages: messages
+            });
+            const response = completion.choices[0].message.content;
+            await interaction.editReply(response);
+        };
+    },
+};
