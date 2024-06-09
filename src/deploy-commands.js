5ccf1218cfc638405e663597d665902c89e4a723
Davi <davifsclmt@gmail.com>
2024-06-09 10:46:31 -0300

    Carregador/Atualizador de comandos otimizado.

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,40 +1,41 @@
 const { REST, Routes } = require('discord.js');
-const { bot_token, clientId, guildId } = require('../src/config.json');
+const { app_token, app_id, app_servers } = require('./configuration/app_config.json');
 
 const fs = require('node:fs');
 const path = require('node:path');
+
 const commands = [];
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	
     for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		
         if ('data' in command && 'execute' in command) {
 			commands.push(command.data.toJSON());
 		} else {
-			console.log(`[AVISO] O comando em '${filePath}' está faltando a propriedade "data" ou "execute".`);
-		};
-	};
-};
-const rest = new REST().setToken(bot_token);
+			console.log(`O comando em '${filePath}' está faltando a propriedade "data" ou "execute".`);
+		}
+	}
+}
+
+const rest = new REST().setToken(app_token);
 
 (async () => {
 	try {
 		console.log(`Recarregando ${commands.length} comandos da aplicação (/).`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId), {
+			Routes.applicationGuildCommands(app_id, app_servers), {
                 body: commands
-            },
+            }
 		);
 		console.log(`${data.length} comandos da aplicação foram recarregados.`);
 	} catch (error) {
-		console.error(`[ERRO]: ${error}`);
-	};
+		console.error(error);
+	}
 })();
