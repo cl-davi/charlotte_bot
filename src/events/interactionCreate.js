98bfff169ef3642aa8a4bcd7344c37df2019f14e
Davi <davifsclmt@gmail.com>
2024-06-09 10:47:28 -0300

    Interação com botões configurado.

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,26 +1,23 @@
 const { Events } = require('discord.js');
+const { verifiedRole } = require('../configuration/app_config.json');
 
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
-
-        const command = interaction.client.commands.get(interaction.commandName);
-
-        if (!command) {
-            console.error(`Nenhum comando chamado ${interaction.commandName} foi encontrado.`);
-            return;
-        };
-
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'Ocorreu um erro enquanto este comando estava sendo executado.', ephemeral: true });
-                await interaction.reply({ content: 'Ocorreu um erro enquanto este comando estava sendo executado', ephemeral: true });
-            };
-        };
-    },
-};
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+            if (!command) {
+                return console.error(`Nenhum comando chamado ${interaction.commandName} foi encontrado.`);
+            }
+            command.execute(interaction);
+        } else if (interaction.isButton()) {
+            const role = interaction.guild.roles.cache.get(verifiedRole);
+            return interaction.member.roles.add(role).then((member) => interaction.reply({
+                content: `O cargo ${role} foi adicionado a você.`,
+                ephemeral: true
+            }));
+        } else {
+            return console.error('Interação indisponível ou não reconhecida.');
+        }
+    }
+}
