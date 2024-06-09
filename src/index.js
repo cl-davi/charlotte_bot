2e64ba33e4eb2950d347eebf76bea942d7b68378
Davi <davifsclmt@gmail.com>
2024-06-09 10:45:30 -0300

    Arquivo principal index.js atualizado.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { bot_token } = require('../src/config.json');
+const { app_token } = require('./configuration/app_config.json');
 
 const fs = require('node:fs');
 const path = require('node:path');
+
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+    intents: [Object.keys(GatewayIntentBits)]
 });
 
 client.commands = new Collection();
@@ -15,31 +16,30 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
-        
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
         } else {
-            console.log(`[AVISO] O comando em '${filePath}' está faltando a propriedade "data" ou "execute".`);
-        };
-    };
-};
+            console.log(`O comando em '${filePath}' está faltando a propriedade "data" ou "execute".`);
+        }
+    }
+}
+
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
     const event = require(filePath);
-    
     if (event.once) {
         client.once(event.name, (...args) => event.execute(...args));
     } else {
         client.on(event.name, (...args) => event.execute(...args));
-    };
-};
+    }
+}
+
 module.exports = client;
 
-client.login(bot_token);
+client.login(app_token);
