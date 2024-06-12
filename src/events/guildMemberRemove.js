a8a960ddfee7bf12a96b4be83446779df0d3cb48
Davi <davifsclmt@gmail.com>
2024-06-12 08:01:03 -0300

    Evento guildMemberRemove pronto

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,19 +1,18 @@
 const { Events, EmbedBuilder } = require('discord.js');
-const { exitChannel } = require('../configuration/app_config.json');
+const { exit_channel_id } = require('../configurations/guild-config.json');
 
 module.exports = {
     name: Events.GuildMemberRemove,
     execute(interaction) {
-        const { guild, user } = interaction;
 
-        const channel = guild.channels.cache.get(exitChannel);
-        const avatar = user.displayAvatarURL({ dynamic: true });
+        const channel = interaction.guild.channels.cache.get(exit_channel_id);
+        const avatar = interaction.user.displayAvatarURL({ dynamic: true });
 
         const embed = new EmbedBuilder()
             .setColor('Random')
             .setTitle('Membro a menos no servidor!')
             .setDescription(`Infelizmente, <@${interaction.id}> saiu do servidor!`)
-            .addFields({ name: 'Número de membros:', value: `${guild.memberCount}` })
+            .addFields({ name: 'Número de membros:', value: `${interaction.guild.memberCount}` })
             .setTimestamp();
 
         channel.send({ embeds: [embed.setThumbnail(avatar)] });
