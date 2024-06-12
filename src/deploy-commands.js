965f7900ef604020184d154ecccabd986ff2ac45
Davi <davifsclmt@gmail.com>
2024-06-12 08:03:03 -0300

    Carregador de comandos configurado

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,41 +1,45 @@
 const { REST, Routes } = require('discord.js');
-const { app_token, app_id, app_servers } = require('./configuration/app_config.json');
+const { token, id, server } = require('./configurations/bot-config.json');
 
 const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
 
+// Pegando todas as subpastas dentro da pasta 'commands'.
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Pegando todos os comandos dentro des subpastas da pasta 'commands'.
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Verificando se os comandos possuem as propriedades 'data' e 'execute'.
     for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
         if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			console.log(`O comando em '${filePath}' está faltando a propriedade "data" ou "execute".`);
-		}
-	}
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`O comando dentro de ${filePath} não possui a propriedade necessária 'data' ou 'execute'.`);
+        }
+    }
 }
 
-const rest = new REST().setToken(app_token);
+// Construindo e preparando uma instância do módulo REST.
+const rest = new REST().setToken(token);
 
+// Fazendo deploy dos comandos.
 (async () => {
-	try {
-		console.log(`Recarregando ${commands.length} comandos da aplicação (/).`);
+    try {
+        console.log(`Fazendo deploy de ${commands.length} comandos (/)...`);
 
-		const data = await rest.put(
-			Routes.applicationGuildCommands(app_id, app_servers), {
-                body: commands
-            }
-		);
-		console.log(`${data.length} comandos da aplicação foram recarregados.`);
-	} catch (error) {
-		console.error(error);
-	}
+        // O método 'put()' está sendo usao para carregar todos os coamndos dentro dentro do servidor.
+        const data = await rest.put(Routes.applicationGuildCommands(id, server), { body: commands });
+        const formatedDataLength = await data.length === 1 ? 'comando foi carregado' : 'comandos foram carregados';
+        console.log(`${data.length} ${formatedDataLength} no servidor.`);
+    } catch (error) {
+        // Pegando os erros de execução, caso o deploy não funcione.
+        console.error(error);
+    }
 })();
