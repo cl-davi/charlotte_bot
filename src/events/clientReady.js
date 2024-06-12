4d4bbb332c164e356c1b9a0745db5788be33a180
Davi <davifsclmt@gmail.com>
2024-06-12 07:59:39 -0300

    Evento clientReady pronto

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -1,9 +1,18 @@
 const { Events } = require('discord.js');
+const { token } = require('../configurations/mongo-config.json');
+
+const mongo = require('mongoose');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
-        console.log(`${client.user.tag} está conectado.`);
+    async execute(client) {
+        try {
+            await mongo.connect(token);
+            console.log('O banco de dados MongoDB está conectado.');
+        } catch (error) {
+            console.error(`Houve um erro ao conectar o banco de dados MongoDB.\n${error}`);
+        }
+        console.log(`O bot ${client.user.tag} está online.`);
     }
 }
