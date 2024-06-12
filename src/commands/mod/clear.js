f8afac83895495fc7a83ad4e54d438430950fa66
Davi <davifsclmt@gmail.com>
2024-06-12 07:53:35 -0300

    Comando de barra '/apagar' concluído.

--- src/commands/mod/clear.js
+++ src/commands/mod/clear.js
@@ -1,63 +1,56 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("limpar")
-        .setDescription("Limpa as mensagens de uma canal de texto ou membro específico.")
+        .setName('apagar')
+        .setDescription('Apaga as mensagens do canal de texto ou de um membro específico.')
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
-        .addIntegerOption(
-            (option) => option
-                .setName('quantidade')
-                .setDescription("Número de mensagens para limpar.")
-                .setMinValue(1)
-                .setMaxValue(90)
-                .setRequired(true)
-        ).addUserOption(
-            (option) => option
-                .setName('membro')
-                .setDescription("Selecione o membro.")
-        ),
+        .addIntegerOption((option) => option.setName('quantidade').setDescription('Número de mensagens para apagar.').setRequired(true).setMinValue(1).setMaxValue(90))
+        .addUserOption((option) => option.setName('membro').setDescription('Selecione o membro.')),
     async execute(interaction) {
-        const amount = interaction.options.getInteger('quantidade');
-        const target = interaction.options.getUser('membro');
-
-        const stringMsg = amount === 1 ? 'mensagem' : 'mensagens';
-        const stringMsgEnd = amount === 1 ? 'foi deletada.' : 'foram deletadas.'
-
         const embed = new EmbedBuilder();
 
-        if (!amount || amount > 90 || amount < 1) {
-            return await interaction.reply({
-                embeds: [embed.setColor('Yellow').setTitle('Valor fora de alcance').setDescription('Apenas valores entre 1 e 90 são permitidos.')],
+        const amount = interaction.options.getInteger('quantidade');
+        const stringMsg = amount === 1 ? 'mensagem' : 'mensagens';
+        const stringMsgFim = amount === 1 ? 'foi deletada.' : 'foram deletadas.'
+        if (amount < 1 || amount > 90) {
+            return interaction.reply({
+                embeds: [embed.setColor('Yellow').setTitle('Quantidade extrapolada').setDescription('Apenas quantidades entre 1 e 90 são permitidas.')],
                 epehemeral: true
             });
         }
 
-        const messagesFromChannel = await interaction.channel.messages.fetch({ limit: amount + 1 });
+        const target = interaction.options.getUser('membro');
 
-        if (target) {
-            let i = 0;
-            const filtered = [];
+        const messagesChannel = await interaction.channel.messages.fetch({ limit: amount + 1 });
 
-            (await messagesFromChannel).filter((msg) => {
-                if (msg.author.id === target.id && amount > i) {
-                    filtered.push(msg);
-                    i++;
-                }
-            });
-            await interaction.channel.bulkDelete(filtered).then(messages => {
-                interaction.reply({
-                    embeds: [embed.setColor('Green').setTitle('Limpeza concluída').setDescription(`${messages.size} ${stringMsg} de ${target} ${stringMsgEnd}`)],
-                    ephemeral: true
+        try {
+            if (target) {
+                let i = 0;
+                const filtered = [];
+
+                (await messagesChannel).filter((msg) => {
+                    if (msg.author.id === target.id && amount > i) {
+                        filtered.push(msg);
+                        i++;
+                    }
                 });
-            });
-        } else {
-            await interaction.channel.bulkDelete(amount, true).then(messages => {
-                interaction.reply({
-                    embeds: [embed.setColor('Green').setTitle('Limpeza concluída').setDescription(`${messages.size} ${stringMsg} ${stringMsgEnd}`)],
-                    ephemeral: true
+                await interaction.channel.bulkDelete(filtered).then(messages => {
+                    interaction.reply({
+                        embeds: [embed.setColor('Aqua').setTitle('Apagado').setDescription(`${messages.size} ${stringMsg} de ${target} ${stringMsgFim}`)],
+                        ephemeral: true
+                    });
                 });
-            });
+            } else {
+                await interaction.channel.bulkDelete(amount, true).then(messages => {
+                    interaction.reply({
+                        embeds: [embed.setColor('Aqua').setTitle('Apagado').setDescription(`${messages.size} ${stringMsg} ${stringMsgFim}`)],
+                        ephemeral: true
+                    });
+                });
+            }
+        } catch (error) {
+            console.error(`Houve um erro ao apagar as mensagens.\n${error}`);
         }
     }
 }
