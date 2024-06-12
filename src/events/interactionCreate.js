4513b0dc373bdca38ae887984183333dea8c1355
Davi <davifsclmt@gmail.com>
2024-06-12 08:01:39 -0300

    Evento interactionCreate pronto

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,23 +1,26 @@
 const { Events } = require('discord.js');
-const { verifiedRole } = require('../configuration/app_config.json');
+const { verified_role_id } = require('../configurations/guild-config.json');
 
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (interaction.isChatInputCommand()) {
-            const command = interaction.client.commands.get(interaction.commandName);
-            if (!command) {
-                return console.error(`Nenhum comando chamado ${interaction.commandName} foi encontrado.`);
+        try {
+            if (interaction.isChatInputCommand()) {
+                const command = interaction.client.commands.get(interaction.commandName);
+                if (!command) {
+                    return console.error(`Nenhum comando chamando ${interaction.commandName} foi encontrado.`);
+                }
+                await command.execute(interaction);
+            } else if (interaction.isButton()) {
+                const role = interaction.guild.roles.cache.get(verified_role_id);
+                await interaction.member.roles.add(role);
+                return interaction.reply({
+                    content: `Você recebeu o cargo ${role}.`,
+                    ephemeral: true
+                });
             }
-            command.execute(interaction);
-        } else if (interaction.isButton()) {
-            const role = interaction.guild.roles.cache.get(verifiedRole);
-            return interaction.member.roles.add(role).then((member) => interaction.reply({
-                content: `O cargo ${role} foi adicionado a você.`,
-                ephemeral: true
-            }));
-        } else {
-            return console.error('Interação indisponível ou não reconhecida.');
+        } catch (error) {
+            console.error(`Houve um erro na interação do comando.\n${error}`);
         }
     }
 }
